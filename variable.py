37e2965c00da6eed1db843348439e86db7ad7a7d
Rachana <acharyarachu108@gmail.com>
2024-10-03 21:56:19 +0530

    commit.py

---
+++ variable.py
@@ -0,0 +1,20 @@
+#Declaring a variable
+
+a=10
+b=20
+
+name="Rachana"
+
+is_switch_on=True
+
+print(a)
+print(b)
+print(name)
+print(is_switch_on)
+
+
+a,b,c=10,20,30
+print(a,b,c)
+
+a=b=c=10
+print(a,b,c)
